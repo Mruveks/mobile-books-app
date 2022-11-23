6c09929f144bbd3514ec8fdbf26f2ebfafeece20
Kuba Mrowiec <mruvekss@gmail.com>
2022-11-23 12:44:13 +0100

    initial commit

---
+++ screens/Home.js
@@ -0,0 +1,12 @@
+import { View, SafeAreaView, FlatList, Text } from 'react-native'
+import { useState } from 'react'
+
+import { COLORS } from '../constants'
+
+const Home = () => {
+    return (
+        <Text>Home</Text>
+    )
+}
+
+export default Home
